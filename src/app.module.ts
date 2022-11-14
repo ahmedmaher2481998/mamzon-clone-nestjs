9e24c10fc8dd150c0812d18f413b9c0506679c4c
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-11-14 18:22:38 +0200

    jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost:2717/amazon'),
     ProductModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/user.types.ts
@@ -0,0 +1,5 @@
+export type userType = {
+  name: string;
+  password: string;
+  email: string;
+};
